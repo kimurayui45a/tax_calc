38e2dda1820c85eb56bedd8e6b12d212ab9fd848
yuikimura <edikanorusiruru@outlook.jp>
2025-05-18 17:28:41 +0900

    消費税計算機

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaxCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcButtonClicked(object sender, EventArgs e)
+        {
+            int price;
+            bool success = int.TryParse(this.priceBox.Text, out price);
+
+            if (success)
+            {
+                // 消費税の計算
+                int taxPrice = (int)(price * 1.1);
+                this.taxPriceBox.Text = taxPrice.ToString();
+            }
+            else 
+            {
+                MessageBox.Show("税抜価格を正しく入力してください");
+            }
+        }
+    }
+}
